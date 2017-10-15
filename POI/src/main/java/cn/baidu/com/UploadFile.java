16c74ed73eb6c88b548afb5fdf2b794dcf2863f8
cgb2499404424@163.com <cgb2499404424@163.com>
2017-10-15 16:39:53 +0800

    代码优化      2017年10月15日16:39:47     home

--- POI/src/main/java/cn/baidu/com/UploadFile.java
+++ POI/src/main/java/cn/baidu/com/UploadFile.java
@@ -1,7 +1,6 @@
 package cn.baidu.com;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,7 +8,7 @@ import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+import cn.baidu.utils.FileUtils;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,6 +19,8 @@ import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.io.IOUtils;
+
+import cn.baidu.utils.UploadUtils;
 
 public class UploadFile extends HttpServlet {
 
@@ -32,43 +33,50 @@ public class UploadFile extends HttpServlet {
 		resp.setCharacterEncoding("utf-8");
 		resp.setContentType("text/html;charset=utf-8");
 		User user = new User();
-		Map map = new HashMap<String,String>();
+		Map map = new HashMap<String, String>();
 		try {
 			DiskFileItemFactory fileItemFactory = new DiskFileItemFactory();
 			ServletFileUpload fileUpload = new ServletFileUpload(fileItemFactory);
 			List<FileItem> list = fileUpload.parseRequest(req);
 			for (FileItem item : list) {
 				String path = null;
-				if (item.isFormField()) {//是表单键值对类型
+				if (item.isFormField()) {// 是表单键值对类型
 					// 普通字段
-					//System.out.println(item.getName());
-					//System.out.println(item.getString("UTF-8"));
-					//System.out.println(item.getFieldName());
+					// System.out.println(item.getName());
+					// System.out.println(item.getString("UTF-8"));
+					// System.out.println(item.getFieldName());
 					map.put(item.getFieldName(), item.getString("UTF-8"));
 				} else {
 					// 文件字段
-					//System.out.println(item.getName()); //文件名
-					//System.out.println(item.getString("UTF-8"));//文件内容
-					//System.out.println(item.getFieldName());//前段传输过程中的的name字段
-					// 获取保存文件的路径
+					// System.out.println(item.getName()); //文件名
+					// System.out.println(item.getString("UTF-8"));//文件内容
+					// System.out.println(item.getFieldName());//前段传输过程中的的name字段
+					// 找到项目根路径下的img文件夹的位置
 					path = getServletContext().getRealPath("/img");
-					File file = new File(path, item.getName());
-					if(!file.exists())//如果问价不存在
-					file.createNewFile();//创建文件
+					// 拼装存储文件的文件夹路径
+					path = path + UploadUtils.getDir(item.getName()).replace("/", "\\")+"\\";
+					System.out.println(path+item.getName());
+					// 创建这个
+					File file = new File(path+item.getName());
+					File dir = new File(path);
+					if(!dir.exists())
+						FileUtils.createDir(path);
+					if (!file.exists())// 如果文件夹不存在
+						file.createNewFile();// 创建文件夹
 					OutputStream out = new FileOutputStream(file);
-					InputStream in = item.getInputStream();//从item中获取输入流
+					InputStream in = item.getInputStream();// 从item中获取输入流
 					IOUtils.copy(in, out);
 					IOUtils.closeQuietly(in);
 					IOUtils.closeQuietly(out);
 				}
-				user.setPart(path+item.getName());
+				user.setPart(path + item.getName());
 			}
 			BeanUtils.populate(user, map);
 			System.out.println(user.toString());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		req.getRequestDispatcher("/jsp/callBack.jsp").forward(req, resp);
+		req.getRequestDispatcher("/callBack.jsp").forward(req, resp);
 	}
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
