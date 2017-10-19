f10b43fb7e4d16248ca5c1442a3efeaba5642f10
cgb2499404424@163.com <cgb2499404424@163.com>
2017-10-19 19:45:16 +0800

    修改文件路径     2017年10月19日19:45:08  home

--- POI/src/main/java/cn/baidu/com/DownloadFile.java
+++ POI/src/main/java/cn/baidu/com/DownloadFile.java
@@ -21,14 +21,14 @@ public class DownloadFile extends HttpServlet {
 	
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String fileName = "C:\\Users\\CGB\\Desktop\\大话数据结构.pdf";
+		String fileName = "大话数据结构.pdf";
 		//设置文件MIME类型  
 		resp.setContentType(getServletContext().getMimeType(fileName)); 
 		
 		//文件下载需要   一个流   两个头
 		ServletOutputStream out = resp.getOutputStream();
 		
-		FileInputStream in = new FileInputStream(fileName);
+		FileInputStream in = new FileInputStream("C:\\Users\\CGB\\Desktop\\"+fileName);
         fileName = FileDownloadUtils.encodeDownloadFilename(fileName, req.getHeader("User-Agent"));
         //设置浏览器打开数据的方式
         resp.setHeader("Content-Disposition", "attachment;filename="+fileName);  
