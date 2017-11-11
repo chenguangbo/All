816bc6d9ca5b6d4028ce4b59fda32bc2de6f2ba7
cgb2499404424@163.com <cgb2499404424@163.com>
2017-11-11 22:17:53 +0800

    Math类    2017年11月11日22:17:48  home

---
+++ Perfect/src/cn/baidu/test/TestMath.java
@@ -0,0 +1,26 @@
+package cn.baidu.test;
+
+public class TestMath {
+
+	public static void main(String[] args) {
+
+		System.out.println(Math.abs(10));//取绝对值
+		System.out.println(Math.abs(-10));//取绝对值
+		
+		System.out.println(Math.ceil(12.2));//向上取整
+		
+		System.out.println(Math.floor(12.5));//向下取整
+		
+		System.out.println(Math.max(14,13));//取最大值
+		
+		System.out.println(Math.min(23, 32));//取最小值
+		
+		System.out.println(Math.pow(12, 2));//乘方
+		
+		System.out.println(Math.random());//产生随机数
+		
+		System.out.println(Math.round(12.2));//四舍五入
+		System.out.println(Math.round(12.5));
+	}
+
+}
