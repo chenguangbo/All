067639c25d3ac3523823814dcb104d2b2ab9cafe
cgb2499404424@163.com <cgb2499404424@163.com>
2017-11-14 20:54:31 +0800

    将日期类型转化成Date类型   2017年11月14日20:54:25  home

--- Perfect/src/cn/baidu/DataMethod/DataFormateMethod.java
+++ Perfect/src/cn/baidu/DataMethod/DataFormateMethod.java
@@ -1,6 +1,7 @@
 package cn.baidu.DataMethod;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -11,19 +12,19 @@ import java.util.Date;
  */
 public class DataFormateMethod {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 
 		Date date = new Date();
 		long time = date.getTime();
 		System.out.println(time);
 		System.out.println(date);
-		
+		//将日期类型转换成字符串
 		DateFormat df = new SimpleDateFormat("yyyy年MM月dd日 HH:mm:ss E");
 		String format = df.format(date);
 		System.out.println(format);
-		
-		
-		
+		//将日期类型转换成Date类型
+		Date parse = df.parse(format);
+		System.out.println(parse);
 		
 		
 		
