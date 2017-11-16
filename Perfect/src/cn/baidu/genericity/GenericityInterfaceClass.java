bd6534635e2359fd95c21bfe9a2ab19bb7ea805e
cgb2499404424@163.com <cgb2499404424@163.com>
2017-11-16 21:30:05 +0800

    泛型接口   2017年11月16日21:30:00   home

--- Perfect/src/cn/baidu/genericity/GenericityInterfaceClass.java
+++ Perfect/src/cn/baidu/genericity/GenericityInterfaceClass.java
@@ -1,5 +1,12 @@
 package cn.baidu.genericity;
 
+/**
+ * 泛型接口
+ * 在创建实现类对象是被确定
+ * 
+ * @author CGB
+ * @param <T>
+ */
 public class GenericityInterfaceClass<T> implements GenericityInterface<T> {
 
 	@Override
