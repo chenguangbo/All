1a3c3c4edad8b0d58c401951f68ff9170d7d47d3
cgb2499404424@163.com <cgb2499404424@163.com>
2017-11-19 22:17:00 +0800

    并发修改异常   2017年11月19日22:16:55  home

---
+++ Perfect/src/cn/baidu/Collection/list/concurrence/TestConcurrence.java
@@ -0,0 +1,12 @@
+package cn.baidu.Collection.list.concurrence;
+
+/**
+ * 并发修改异常
+ * 		指在迭代器遍历集合时改变集合的长度,就会报并发修改异常
+ * 		而且在修改集合中倒数第二个的时候  有可能不报这个异常
+ * @author CGB
+ *
+ */
+public class TestConcurrence {
+
+}
