1b07c75faa45d6f94bc24fd8ff1c3bfad5deba36
cgb2499404424@163.com <cgb2499404424@163.com>
2017-11-24 22:07:45 +0800

    Set接口测试   2017年11月24日22:07:39  home

--- Perfect/src/cn/baidu/Collection/set/TestSet.java
+++ Perfect/src/cn/baidu/Collection/set/TestSet.java
@@ -1,5 +1,7 @@
 package cn.baidu.Collection.set;
 
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * set接口特点              不可重复      没有索引
@@ -10,6 +12,13 @@ public class TestSet {
 
 	public static void main(String[] args) {
 		
+		Set<String> set = new HashSet<String>();
+		set.add("你好");
+		set.add("啊");
+		set.add("Siri");
+		for (String string : set) {
+			System.out.println(string);
+		}
 		
 		
 	}
