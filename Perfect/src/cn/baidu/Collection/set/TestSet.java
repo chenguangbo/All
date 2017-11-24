bf6c0818f1715799eff4ada3659588e63cbf5285
cgb2499404424@163.com <cgb2499404424@163.com>
2017-11-24 21:52:15 +0800

    set接口 2017年11月24日21:52:10  home

--- Perfect/src/cn/baidu/Collection/set/TestSet.java
+++ Perfect/src/cn/baidu/Collection/set/TestSet.java
@@ -1,5 +1,19 @@
 package cn.baidu.Collection.set;
 
+
+/**
+ * set接口特点              不可重复      没有索引
+ * @author CGB
+ *
+ */
 public class TestSet {
 
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+	
+	
 }
