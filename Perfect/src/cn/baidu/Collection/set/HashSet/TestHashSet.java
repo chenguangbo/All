43c03199f51abff06580efe98874932ca88ae4ff
cgb2499404424@163.com <cgb2499404424@163.com>
2017-11-25 21:42:01 +0800

    HashSet集合   2017年11月25日21:41:55    home

--- Perfect/src/cn/baidu/Collection/set/HashSet/TestHashSet.java
+++ Perfect/src/cn/baidu/Collection/set/HashSet/TestHashSet.java
@@ -12,6 +12,12 @@ import java.util.HashSet;
  *      				调用equals 比较内容 
  *      					内容相同 不添加 
  *      					内容不同  添加 	
+ *      
+ *      
+ *      
+ *      此类实现Set接口，由哈希表支持（实际上是一个 HashMap集合 (HashMap的kye)）。HashSet集合不能保证的迭代顺序与元素存储顺序相同
+ *      HashSet集合，采用哈希表结构存储数据，保证元素唯一性的方式依赖于：hashCode()与equals()方法。
+ *      
  */
 public class TestHashSet {
 
