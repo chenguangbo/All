5861f0b8caa00b54ef178a3cf082ba90ef7bd93f
cgb2499404424@163.com <cgb2499404424@163.com>
2017-11-27 21:46:00 +0800

    双向链表    2017年11月27日21:45:55   home

---
+++ Perfect/src/cn/baidu/Collection/set/LinkedHashSet/TestLinkedHashSet.java
@@ -0,0 +1,30 @@
+package cn.baidu.Collection.set.LinkedHashSet;
+
+import java.util.LinkedHashSet;
+
+
+/**
+ * 
+ * 		有序     不可重复(唯一   用equals和HashCode判断)  没有索引
+ * 
+ *     	数据结构   :   双向链表
+ *     
+ * 2017年11月27日21:43:47
+ * @author CGB
+ *
+ */
+public class TestLinkedHashSet {
+
+	
+	public static void main(String[] args) {
+		
+		
+		LinkedHashSet<String> set = new LinkedHashSet<String>();
+		
+		
+		
+	}
+	
+	
+	
+}
