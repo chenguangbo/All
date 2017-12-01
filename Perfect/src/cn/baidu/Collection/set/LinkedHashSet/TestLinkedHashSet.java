356387f8aa33f731d6245ce725eca23b0a7fedf8
cgb2499404424@163.com <cgb2499404424@163.com>
2017-12-01 20:44:31 +0800

    LinkedHashSet   2017年12月1日20:44:19   home

--- Perfect/src/cn/baidu/Collection/set/LinkedHashSet/TestLinkedHashSet.java
+++ Perfect/src/cn/baidu/Collection/set/LinkedHashSet/TestLinkedHashSet.java
@@ -7,7 +7,7 @@ import java.util.LinkedHashSet;
  * 
  * 		有序     不可重复(唯一   用equals和HashCode判断)  没有索引
  * 
- *     	数据结构   :   双向链表
+ *     	数据结构   :   链表
  *     
  * 2017年11月27日21:43:47
  * @author CGB
@@ -17,7 +17,6 @@ public class TestLinkedHashSet {
 
 	
 	public static void main(String[] args) {
-		
 		
 		LinkedHashSet<String> set = new LinkedHashSet<String>();
 		
