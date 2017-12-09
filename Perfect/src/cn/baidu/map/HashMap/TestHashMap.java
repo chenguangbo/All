aa48ba69c76744cf2e43f9846e7af0e300cfba3d
cgb2499404424@163.com <cgb2499404424@163.com>
2017-12-09 22:25:34 +0800

    Map的keySet遍历    2017年12月9日22:25:30   home

--- Perfect/src/cn/baidu/map/HashMap/TestHashMap.java
+++ Perfect/src/cn/baidu/map/HashMap/TestHashMap.java
@@ -2,6 +2,7 @@ package cn.baidu.map.HashMap;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class TestHashMap {
 
@@ -12,12 +13,26 @@ public class TestHashMap {
 		map.put("愛", "爱");
 		map.put("XX", "xx");
 		map.put("....", "....");
-		
-		String key = map.get("愛");
-		System.out.println(key);
-		
+
+		String key1 = map.get("愛");
+		System.out.println(key1);
+
 		String remove = map.remove("....");
-		System.out.println("被刪除的數據是:  "+remove);
+		System.out.println("被刪除的數據是:  " + remove);
+
+		System.out.println("map遍历部分------------------------------");
+		
+		// Map便利
+		Set<String> keySet = map.keySet();
+		for (String key : keySet) {
+			String value = map.get(key);
+			System.out.println(key + " : " + value);
+		}
+		
+		
+		
+		
+
 	}
 
 }
