277e6a190dd5f7b56b42ffe2ee49fb4f7fd566b9
cgb2499404424@163.com <cgb2499404424@163.com>
2017-12-13 21:18:32 +0800

    Exception异常           2017年12月13日21:18:28   home

---
+++ Perfect/src/cn/baidu/Exception/TestError.java
@@ -0,0 +1,14 @@
+package cn.baidu.Exception;
+
+public class TestError {
+
+	public static void main(String[] args) {
+		
+		int[] arr = new int[1024*1024*42];
+		System.out.println(arr);
+		
+		
+		
+	}
+
+}
