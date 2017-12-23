9fd1f89ecccab81e15a75c4bf3ca0ecf1c25fc10
cgb2499404424@163.com <cgb2499404424@163.com>
2017-12-23 23:08:57 +0800

    正则表达式        home     2017年12月23日23:08:55

--- Perfect/src/cn/baidu/regex/Regex.java
+++ Perfect/src/cn/baidu/regex/Regex.java
@@ -3,8 +3,13 @@ package cn.baidu.regex;
 public class Regex {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String regex = "^http://[a-zA-Z_0-9]+[>]$";
+		String str = "<script type='textjavascript src=http://gss0.baidu.com/70cFsjip0QIZ8tyhnq/hunter/baike.js?st=17521 defer></script><script type=text/javascript>alog('speed.set', 'drt', +new Date)";
+
+		System.out.println(str.matches(regex));
+		
+		
 	}
 
 }
