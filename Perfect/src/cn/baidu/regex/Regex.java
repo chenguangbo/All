f0d867709948ea6497c37b2b61aca786ad137465
cgb2499404424@163.com <cgb2499404424@163.com>
2017-12-28 17:52:52 +0800

    2017年12月28日17:52:50

--- Perfect/src/cn/baidu/regex/Regex.java
+++ Perfect/src/cn/baidu/regex/Regex.java
@@ -4,8 +4,8 @@ public class Regex {
 
 	public static void main(String[] args) {
 
-		String regex = "^http://[a-zA-Z_0-9]+[>]$";
-		String str = "<script type='textjavascript src=http://gss0.baidu.com/70cFsjip0QIZ8tyhnq/hunter/baike.js?st=17521 defer></script><script type=text/javascript>alog('speed.set', 'drt', +new Date)";
+		String regex = "^http://[a-zA-Z_0-9]+>$";
+		String str = "<script type='textjavascript src=http://gss0_baidu_com>/70cFsjip0QIZ8tyhnq/hunter/baike.js?st=17521 defer></script><script type=text/javascript>alog('speed.set', 'drt', +new Date)";
 
 		System.out.println(str.matches(regex));
 		
