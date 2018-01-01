6531fd2d6dadc69f426c3e4c0ce24943c8058a49
cgb2499404424@163.com <cgb2499404424@163.com>
2018-01-01 13:39:45 +0800

    listFile和list方法   2018年1月1日13:39:41   home

--- Perfect/src/cn/baidu/File/TestDir.java
+++ Perfect/src/cn/baidu/File/TestDir.java
@@ -1,5 +1,28 @@
 package cn.baidu.File;
 
+import java.io.File;
+
+/*
+ * 遍历文件夹
+ */
+
 public class TestDir {
 
+	
+	public static void main(String[] args) {
+		
+		File file = new File("C:/Users/CGB/Desktop");
+		String[] list = file.list();
+		//得到相对路径
+		for (String string : list) {
+			System.out.println(string);
+		}
+		System.out.println("-----------------------------------");
+		//得到全路径
+		File[] listFiles = file.listFiles();
+		for (File file2 : listFiles) {
+			System.out.println(file2);
+		}
+		
+	}
 }
