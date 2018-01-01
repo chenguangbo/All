7e8850cf100af3727408bd5e046afe8a68a62fa4
cgb2499404424@163.com <cgb2499404424@163.com>
2018-01-01 22:22:47 +0800

    FileOutputStream   2018年1月1日22:22:42  home

--- Perfect/src/cn/baidu/File/FileOutputStream/OutPutStream.java
+++ Perfect/src/cn/baidu/File/FileOutputStream/OutPutStream.java
@@ -10,7 +10,12 @@ public class OutPutStream {
 		out.write("新年快乐".getBytes());
 		out.flush();
 		out.close();
-		
+		//在原有数据上追加
+		FileOutputStream out2 = new FileOutputStream("新年快乐.txt",true);
+		out2.write("\r\n".getBytes());
+		out2.write("新年快乐兔".getBytes());
+		out2.flush();
+		out2.close();
 	}
 
 }
