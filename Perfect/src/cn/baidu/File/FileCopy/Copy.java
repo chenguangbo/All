ba721db5d07a6e4aad35e556dffa00cc0e192343
cgb2499404424@163.com <cgb2499404424@163.com>
2018-01-04 23:01:55 +0800

    编码方式    2018年1月4日23:01:49    home

---
+++ Perfect/src/cn/baidu/File/FileCopy/Copy.java
@@ -0,0 +1,15 @@
+package cn.baidu.File.FileCopy;
+
+import java.util.Arrays;
+
+public class Copy {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Arrays.toString(("�").getBytes()));
+		
+		
+	}
+	
+	
+}
