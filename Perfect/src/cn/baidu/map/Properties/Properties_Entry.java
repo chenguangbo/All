8d3cfca63085352694b3eebe8bff107a9545fb3b
cgb2499404424@163.com <cgb2499404424@163.com>
2018-01-12 21:56:23 +0800

    集合遍历  2018年1月12日21:56:17   home

--- Perfect/src/cn/baidu/map/Properties/Properties_Entry.java
+++ Perfect/src/cn/baidu/map/Properties/Properties_Entry.java
@@ -3,19 +3,21 @@ package cn.baidu.map.Properties;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Map.Entry;
 import java.util.Properties;
+import java.util.Set;
 
-public class Reader_Properties {
+public class Properties_Entry {
 
 	public static void main(String[] args) throws IOException {
-		
 		Properties properties = new Properties();
 		Reader reader = new FileReader("喜欢你.txt");
 		properties.load(reader);
-		String name = properties.getProperty("你好");
-		System.out.println(name);
-		System.out.println(properties);
-			
+		Set<Entry<Object,Object>> entrySet = properties.entrySet();
+		for(Entry<Object,Object> entry : entrySet){
+			System.out.println(entry.getKey()+":"+entry.getValue());
+		}
+		System.out.println(entrySet);
 	}
 
 }
